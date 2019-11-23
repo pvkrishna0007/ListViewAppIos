0845157acf91344c5c67ab226dc3b06d0fba22fd
Krishna0007 <pvkrishna0007@gmail.com>
2019-11-23 14:52:42 +0530

    movable items in table view

--- ListViewApp/StoriesTableTableViewController.swift
+++ ListViewApp/StoriesTableTableViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+class HeadLineTableViewCell : UITableViewCell{
+    @IBOutlet weak var HeadLineTitle: UILabel!
+    @IBOutlet weak var HeadLineSubTitle: UILabel!
+    @IBOutlet weak var HeadLineImage: UIImageView!
+    
+}
+
 class StoriesTableTableViewController: UITableViewController {
 
     struct HeadLine {
@@ -16,11 +23,16 @@ class StoriesTableTableViewController: UITableViewController {
     }
     
     var headlines = [
-        HeadLine(name:"Fruits", items: ["Apple", "Orange", "Mango"]),
-        HeadLine(name:"Colors", items: ["Red", "Yellow", "Pink", "Cyan"])
+        HeadLine(name:"Fruits", items: ["Apple", "Banana", "Mango","Sapota", "Orange", "Gova"]),
+        HeadLine(name:"Colors", items: ["Red", "Yellow", "Pink", "Cyan", "Purple", "Green"])
     ]
     // MARK: - Table view data source
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.isEditing = true
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return headlines.count
     }
@@ -30,13 +42,17 @@ class StoriesTableTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath) as! HeadLineTableViewCell
 
         let model = headlines[indexPath.section].items[indexPath.row]
-        let lbText = "\(model)"
-        cell.textLabel?.text = lbText
-        cell.detailTextLabel?.text = "mySub"
-        cell.imageView?.image = UIImage(named: model)
+        let lbText = "\(model) hjhjjkkkj jjkjkj kknnk kkk"
+//        cell.textLabel?.text = lbText
+//        cell.detailTextLabel?.text = "mySub"
+//        cell.imageView?.image = UIImage(named: model)
+        cell.HeadLineTitle.text = lbText
+        cell.HeadLineSubTitle.text = "MySubtitle"
+        cell.HeadLineSubTitle.isHidden = true
+        cell.HeadLineImage.image = UIImage(named: model)
         return cell
     }
     
@@ -44,5 +60,17 @@ class StoriesTableTableViewController: UITableViewController {
         return "\(headlines[section].name)"
     }
     
-    override func sel
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let moving = headlines[sourceIndexPath.section].items[sourceIndexPath.row];
+        headlines[sourceIndexPath.section].items.remove(at: sourceIndexPath.row)
+        headlines[destinationIndexPath.section].items.insert(moving, at: destinationIndexPath.row)
+    }
 }
